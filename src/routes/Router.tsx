6d2e3da1c8e13e1bb848623ef52f73f162ebc36e
trikitirem <trikitirem@outlook.com>
2022-01-22 00:16:12 +0100

    tailwind setup, supabase setup

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route } from "wouter";
+import { supabase } from "../supabaseClient";
+import Welcome from "./welcome/Welcome";
 
 const Router: React.FC = () => {
-  return <div className="text-red-500">Router</div>;
+  const [signedIn, setSignedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (supabase.auth.user()) {
+      setSignedIn(true);
+    } else {
+      setSignedIn(false);
+    }
+  }, []);
+
+  return (
+    <div>
+      <Route path="/">{signedIn ? <div>Home</div> : <Welcome />}</Route>
+    </div>
+  );
 };
 
 export default Router;
